04c4bcd8091f0a38fcdd4d01dc89200993ba57ca
laladwesh <guptaavinash302@gmail.com>
2025-06-15 11:50:41 +0530

    changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,6 +10,8 @@ import {
   reactjs,
   redux,
   tailwind,
+  techboard,
+  kriti,
   nodejs,
   mongodb,
   git,
@@ -17,10 +19,13 @@ import {
   docker,
   meta,
   starbucks,
+  ecell,
+  spirit,
   tesla,
   shopify,
   carrent,
   jobit,
+  icacc2025,
   tripguide,
   threejs,
 } from "../assets";
@@ -116,10 +121,10 @@ const technologies = [
 
 const experiences = [
   {
-    title: "Web Developer",
+    title: "Senior Web Developer",
     company_name: "SWC IIT Guwahati",
     icon: starbucks,
-    iconBg: "#383E56",
+    iconBg: "#000",
     date: "April 2024 - Present",
     points: [
       "Developing and maintaining backend systems for the college's app to ensure seamless functionality and performance.",
@@ -129,11 +134,37 @@ const experiences = [
     ],
   },
   {
-    title: "Web-Operations Executive",
+    title: "Web Operations Head",
+    company_name: "Spirit IIT Guwahati",
+    icon: spirit,
+    iconBg: "#000",
+    date: "April 2025 - Present",
+    points: [
+      "Overseeing development and deployment of Spirit IITG website, ensuring uptime and seamless user experience.",
+      "Coordinating with design and other teams to implement real-time event schedules and interactive fixtures.",
+      "Managing server infrastructure and CI/CD pipelines to guarantee efficient updates and secure data handling.",
+      "Monitoring website analytics and performance metrics to optimize load times and enhance participant engagement.",
+    ],
+  },
+    {
+    title: "Web-Operations Head",
+    company_name: "ICACC 2025",
+    icon: spirit ,
+    iconBg: "#fff",
+    date: "September 2024 - May 2025",
+   points: [
+    "3rd International Conference on Agriculture Centric Computation (ICACC) 2025.",
+    "Single-handedly managed end-to-end development and deployment of the conference’s website, building it from scratch and hosting it on the IITG server throughout the tenure.",
+    "Conducted regular code reviews to enforce best practices and elevate code quality across the web team.",
+    "Optimized website performance and implemented security measures to ensure reliability and data integrity.",
+    ],
+  },
+  {
+    title: "Web Operations Head",
     company_name: "Cepstrum IIT Guwahati",
     icon: tesla,
     iconBg: "#E6DEDD",
-    date: "August 2024 - Present",
+    date: "June 2025 - Present",
     points: [
       "Developing and maintaining web portals and applications for the institute using modern web technologies.",
       "    Overseeing the management and optimization of the institute's website to ensure its smooth operation and reliability.",
@@ -154,19 +185,19 @@ const experiences = [
       "Ensuring responsive and interactive design elements for an engaging user experience across all devices.",
     ],
   },
-  {
-    title: "Co-Ordinator",
-    company_name: "Coding Club IIT Guwahati",
-    icon: meta,
-    iconBg: "#383E56",
-    date: "Dec 2024 - Present",
-    points: [
-      "Organizing and conducting coding workshops to educate and inspire students on various programming concepts and technologies.",
-      "Collaborating with team members and mentors to plan events and create engaging learning experiences.",
-      "Developing and maintaining resources for workshops, ensuring accessibility and practical applicability for participants.",
-      "Providing mentorship and support to participants, fostering a collaborative and innovative environment.",
-    ],
-  },
+  // {
+  //   title: "Co-Ordinator",
+  //   company_name: "Coding Club IIT Guwahati",
+  //   icon: meta,
+  //   iconBg: "#383E56",
+  //   date: "Dec 2024 - May 2025",
+  //   points: [
+  //     "Organizing and conducting coding workshops to educate and inspire students on various programming concepts and technologies.",
+  //     "Collaborating with team members and mentors to plan events and create engaging learning experiences.",
+  //     "Developing and maintaining resources for workshops, ensuring accessibility and practical applicability for participants.",
+  //     "Providing mentorship and support to participants, fostering a collaborative and innovative environment.",
+  //   ],
+  // },
 ];
 
 const testimonials = [
@@ -176,7 +207,24 @@ const testimonials = [
     name: "Hacktoberfest",
     designation: "SWC",
     company: "IITG",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGkuDTSquSz3iNxPdrMcZDtMN4eVnPH9Hj1g&s",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGkuDTSquSz3iNxPdrMcZDtMN4eVnPH9Hj1g&s",
+  },
+    {
+    testimonial:
+      "Achieved third place in Inter-IIT Bootcamp Software-Dev PS, showcasing exceptional problem-solving skills.",
+    name: "Inter-IIT Bootcamp",
+    designation: "Technical Board",
+    company: "IITG",
+    image: techboard,
+  },
+   {
+    testimonial:
+      "Secured first place in Product Development, highlighting outstanding problem-solving abilities.",
+    name: "Kriti",
+    designation: "Technical Board",
+    company: "IITG",
+    image: kriti,
   },
   {
     testimonial:
@@ -184,7 +232,8 @@ const testimonials = [
     name: "C++ Programming",
     designation: "Certification",
     company: "Udemy",
-    image: "https://img.utdstc.com/icon/ff0/a5e/ff0a5ec61bc652cce9761f4e68089610662e76fcfea8c84fa813e10d3a9ed3d7:200",
+    image:
+      "https://img.utdstc.com/icon/ff0/a5e/ff0a5ec61bc652cce9761f4e68089610662e76fcfea8c84fa813e10d3a9ed3d7:200",
   },
   {
     testimonial:
@@ -192,11 +241,37 @@ const testimonials = [
     name: "HackStack",
     designation: "SWC",
     company: "IITG",
-    image: "https://play-lh.googleusercontent.com/w0scr6QhQ8YRcp5bnQ1HRuOUWA0ZE5TlN2Pt66TYjCDZZ5-kxUqPy_RnoJfg-jhFuBg=w600-h300-pc0xffffff-pd",
+    image: starbucks,
   },
+  {
+    testimonial:
+      "Participated and Completed the EnCode 2025 - Code to Innovate Hackathon, Organized by E-Cell, IITG.",
+    name: "EnCode 2025",
+    designation: "E-Cell",
+    company: "IITG",
+    image: ecell,
+  },
+
 ];
 
 const projects = [
+    {
+    name: "ICACC 2025",
+    description:
+      "A dynamic web-based portal powering ICACC 2025, serving as the central hub for organizers, speakers, sponsors, and attendees. Organizers can manage calls for papers, registrations, session scheduling, and announcements, while speakers submit abstracts, update profiles, and upload presentation materials. Sponsors gain dedicated showcase pages, and attendees can explore the program, register for workshops, and network—all through a responsive, secure, and intuitive interface.",
+    tags: [
+      {
+        name: "reactjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "green-text-gradient",
+      },
+    ],
+    image: icacc2025,
+    source_code_link: "https://github.com/laladwesh/ICA25",
+  },
   {
     name: "Job Portal",
     description:
@@ -256,7 +331,6 @@ const projects = [
     description:
       "A feature-rich food delivery platform with dedicated portals for admins and customers. Customers can browse menus, customize orders, and track their pizza deliveries in real time. Admins manage orders, update menus, and oversee delivery logistics, ensuring a seamless experience. The personalized recommendations, and notifications, making it the ultimate solution for pizza lovers and restaurant managers alike!",
     tags: [
-      
       {
         name: "ejs",
         color: "yellow-text-gradient",
